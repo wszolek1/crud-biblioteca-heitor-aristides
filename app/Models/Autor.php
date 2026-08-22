696b18ac448c9bd619f0714b410c318a151fad3e
wszolek1 <wszolek.heitor@gmail.com>
2026-08-22 19:47:29 -0300

    implementados os models

--- app/Models/Autor.php
+++ app/Models/Autor.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Autor extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'nacionalidade',
+        'data_nascimento',
+        'biografia',
+    ];
+
+    // Relacionamento: um Autor tem vários Livros
+    public function livros(): HasMany
+    {
+        return $this->hasMany(Livro::class);
+    }
 }
