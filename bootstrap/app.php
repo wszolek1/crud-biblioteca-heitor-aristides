5d4cb0c077e7066a65fb4d2cc8f0716348ea2d50
AristidesMoreira <20251ctb0100063@estudantes.ifpr.edu.br>
2026-08-23 15:46:46 -0300

    criação do service e middlewares registrados

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
+->withMiddleware(function (Middleware $middleware) {
+    $middleware->alias([
+        'checkIsLogged' => \App\Http\Middleware\CheckIsLogged::class,
+        'checkIsNotLogged' => \App\Http\Middleware\CheckIsNotLogged::class,
+    ]);
+})
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
