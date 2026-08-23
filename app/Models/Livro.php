0ef51dc4abc831d06f6722de81e9f359a81edf1a
wszolek1 <wszolek.heitor@gmail.com>
2026-08-23 15:02:36 -0300

    criação dos models

--- app/Models/Livro.php
+++ app/Models/Livro.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Livro extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'titulo',
+        'genero',
+        'ano_publicacao',
+        'quantidade_estoque',
+        'autor_id',
+    ];
+
+    // Relacionamento: um Livro pertence a um Autor
+    public function autor(): BelongsTo
+    {
+        return $this->belongsTo(Autor::class);
+    }
 }
